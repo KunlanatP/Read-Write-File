b9b2754b4ee63fda6b70c993bb7ec593caf5e1f9
KunlanatP <kunlanat.pakine@gmail.com>
2022-09-03 23:37:35 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io' as io;
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,109 +10,176 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Read/Write',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String fullPath =
+      '/Users/kunlanat.p/KUNLANAT/Flutter-Dev/Read-Write File/read_write_file_example/lib/AndroidManifest.xml';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String oldText = '';
+  String newText = '';
+
+  @override
+  void initState() {
+    debugPrint('[LOG] :: initState');
+    _read();
+    super.initState();
   }
+
+  Future<void> _read() async {
+    try {
+      if (kIsWeb) {
+      } else {
+        final io.File file = io.File(fullPath);
+        String text = await file.readAsString();
+        setState(() => oldText = text);
+      }
+    } catch (e) {
+      debugPrint("Couldn't read file :: $e");
+    }
+  }
+
+  // _downloadFile(String path) {
+  //   AnchorElement anchorElement = AnchorElement(href: path);
+  //   anchorElement.download = 'Download';
+  //   anchorElement.click();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final decoration = BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(4, 4),
+            blurRadius: 15,
+            spreadRadius: 1,
+            color: Colors.grey.shade800,
+          ),
+          const BoxShadow(
+            offset: Offset(-4, -4),
+            blurRadius: 15,
+            spreadRadius: 1,
+            color: Colors.white,
+          ),
+        ]);
+
+    final child = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          'Android Package Kit',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        const Divider(thickness: 2, color: Colors.black38),
+        const SizedBox(height: 20),
+        SizedBox(
+          height: 42,
+          child: TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'NAME',
+            ),
+            onSubmitted: (value) {},
+          ),
+        ),
+        const SizedBox(height: 20),
+        SizedBox(
+          height: 42,
+          child: TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'PACKAGE',
+            ),
+            onSubmitted: (value) {},
+          ),
+        ),
+        const SizedBox(height: 20),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: ElevatedButton(
+            onPressed: () => _showMyDialog(),
+            style: ElevatedButton.styleFrom(
+              fixedSize: const Size(300, 40),
+              backgroundColor: Colors.blueAccent,
+            ),
+            child: const Text('Compare'),
+          ),
+        )
+      ],
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: Colors.grey.shade300,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Container(
+          width: 500,
+          height: 300,
+          decoration: decoration,
+          padding: const EdgeInsets.all(30),
+          child: child,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Compare'),
+          content: SingleChildScrollView(
+            child: Row(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width / 2 - 200,
+                  height: 600,
+                  color: Colors.amber,
+                  child: SingleChildScrollView(child: Text(oldText)),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 2 - 200,
+                  height: 600,
+                  color: Colors.green,
+                  child: SingleChildScrollView(child: Text(newText)),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Approve'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
